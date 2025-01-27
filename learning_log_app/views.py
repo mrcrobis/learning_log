ac02080a6455447fbf73da119720e03a68d9925d
Joao Vitor <joaovrsilveira16@gmail.com>
2025-01-27 18:35:44 -0300

    Página para adicionar outros tópicos

--- learning_log_app/views.py
+++ learning_log_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from . models import Topic
+from . forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     return render(request, 'learning_log_app/index.html')
@@ -14,3 +17,15 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_log_app/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+        
+    context = {'form': form}
+    return render(request, 'learning_log_app/new_topic.html', context)
